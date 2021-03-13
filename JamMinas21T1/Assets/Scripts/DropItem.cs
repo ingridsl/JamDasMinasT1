af1c87d3dc600a160d04d3a53b6ce6ff4ed8138b
ingridsl <inus2agome@gmail.com>
2021-03-13 00:06:49 -0300

    drop item

---
+++ JamMinas21T1/Assets/Scripts/DropItem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItem : MonoBehaviour
+{
+
+    public Sprite dropItem = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                this.GetComponent<SpriteRenderer>().sprite = dropItem;
+            }
+        }
+    }
+}
