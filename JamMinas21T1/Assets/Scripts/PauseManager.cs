fbacc42d729548bf564cca27b91a0d67acf6928d
ingridsl <inus2agome@gmail.com>
2021-03-13 10:25:28 -0300

    botão pause e organização de HUD

--- JamMinas21T1/Assets/Scripts/PauseManager.cs
+++ JamMinas21T1/Assets/Scripts/PauseManager.cs
@@ -7,35 +7,38 @@ public class PauseManager : MonoBehaviour
 {
     public Slider movementSlider = null;
     public GameManager game = null;
-    bool paused = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (game.wasdMovement != (movementSlider.value == 0)) {
+        if (game.wasdMovement != (movementSlider.value == 0))
+        {
             game.changeMovementButton(movementSlider.value == 0);
         }
 
         if (Input.GetKeyDown(KeyCode.P))
         {
-            var child = this.transform.GetChild(0);
-            if (child.gameObject.activeSelf && !paused)
-            {
-                paused = true;
-                child.gameObject.SetActive(false);
-                Time.timeScale = 1;
-            }
-            else
-            {
-                paused = false;
-                child.gameObject.SetActive(true);
-                Time.timeScale = 0;
-            }
+            OpenClosePause();
+        }
+    }
+
+    public void OpenClosePause()
+    {
+        var child = this.transform.GetChild(0);
+        if (child.gameObject.activeSelf)
+        {
+            child.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+        else
+        {
+            child.gameObject.SetActive(true);
+            Time.timeScale = 0;
         }
     }
 }
