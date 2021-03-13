32da2b0a5ed819f710a7ed1388a2ed9ae179bfd9
ingridsl <inus2agome@gmail.com>
2021-03-13 00:53:52 -0300

    setting new goals

---
+++ JamMinas21T1/Assets/Scripts/Constants.cs
@@ -0,0 +1,12 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class Constants
+{
+    public static readonly float BASIC_COUNTDOWN_TIME = 50f;
+    public static readonly int BASIC_ENEMY_GOLD = 1000;
+
+}
--- JamMinas21T1/Assets/Scripts/Goal.cs
+++ JamMinas21T1/Assets/Scripts/Goal.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Goal : MonoBehaviour
 {
+
+    int enemyGold = Constants.BASIC_ENEMY_GOLD;
+    public bool achieved = false;
+    Countdown contdown = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,21 @@ public class Goal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (achieved)
+        {
+            SetGoal();
+        }
         
     }
+
+    void SetGoal()
+    {
+        enemyGold *= (int) 1.5f;
+        int playerGold = enemyGold;//valor provisorio pegar o dinheiro do player
+        float extraTime = playerGold / enemyGold > 1.5 ? -10 : 10;
+
+        contdown.startingTime += extraTime;
+        contdown.Restart(contdown.startingTime);
+    }
+
 }
