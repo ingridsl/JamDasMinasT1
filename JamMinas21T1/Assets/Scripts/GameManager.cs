a148918ff68967c01aefc246831afce5464b979d
ingridsl <inus2agome@gmail.com>
2021-03-14 16:00:31 -0300

    BEAUTIFUL BUILD!
    stone
    exit
    build

--- JamMinas21T1/Assets/Scripts/GameManager.cs
+++ JamMinas21T1/Assets/Scripts/GameManager.cs
@@ -23,7 +23,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
     }
 
     public void RestartStones(int quantity)
