fe234a61c45a137957fcfcfb92d53d36341db85d
ingridsl <inus2agome@gmail.com>
2021-03-14 00:31:01 -0300

    player walking anim (almost)

--- JamMinas21T1/Assets/Scripts/PlayerManager.cs
+++ JamMinas21T1/Assets/Scripts/PlayerManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public Animator animator = null;
     public float moveSpeed = 1f;
     public GameManager gameManager = null;
 
@@ -22,6 +23,7 @@ public class PlayerManager : MonoBehaviour
 
     void Movement()
     {
+
         Vector3 moveInput = Vector3.zero;
         if (Input.GetKey(gameManager.left) || Input.GetKey(gameManager.right))
         {
@@ -32,6 +34,10 @@ public class PlayerManager : MonoBehaviour
         {
             moveInput.y = Input.GetAxis("Vertical");
         }
+
+        animator.SetFloat("Horizontal", moveInput.x);
+        animator.SetFloat("Vertical", moveInput.y);
+        animator.SetFloat("Speed", moveInput.magnitude);
         transform.position += moveInput * moveSpeed * Time.deltaTime;
     }
 }
