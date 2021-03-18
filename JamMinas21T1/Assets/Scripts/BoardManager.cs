be1aeae6e8b9b19aad206efccad69bf86ed2f726
ingridsl <inus2agome@gmail.com>
2021-03-18 17:45:41 -0300

    placa só abre quando clica com mouse ou espaço

--- JamMinas21T1/Assets/Scripts/BoardManager.cs
+++ JamMinas21T1/Assets/Scripts/BoardManager.cs
@@ -6,12 +6,26 @@ public class BoardManager : MonoBehaviour
 
     public GameObject _board;
 
+    bool isInside = false;
+
+    void Update()
+    {
+        if (isInside && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space) && !_board.gameObject.activeSelf))
+        {
+            OpenBoard();
+        }
+        else if(Input.GetKeyDown(KeyCode.Space) && _board.gameObject.activeSelf)
+        {
+            close();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         var target = other.gameObject;
         if (target.CompareTag("Player"))
         {
-            OpenBoard();
+            isInside = true;
         }
     }
 
@@ -25,5 +39,23 @@ public class BoardManager : MonoBehaviour
     {
         _board.gameObject.SetActive(false);
         Time.timeScale = 1;
+        isInside = false;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInside = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isInside = false;
+        }
     }
 }
